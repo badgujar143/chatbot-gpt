36636204b2c37ef2fdd53225a2ceb9a095b91afc
Gaurav Badgujar <gaurav.badgujar@buenofinanceinc.com>
2023-01-21 09:19:45 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,8 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            // "https://chatgpt-ai-app-od21.onrender.com",
-            "http://localhost:5173",
+            "https://chatbot-gpt.onrender.com/",
+            // "http://localhost:5173",
             { input },
             {
                 headers: {
